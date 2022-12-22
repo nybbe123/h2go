e7d05051b7595d06783042e2d95f318007e58610
Emil <emil.hagelin@medieinstitutet.se>
2022-12-22 10:46:42 +0100

    testing

--- pages/landingPage.tsx
+++ pages/landingPage.tsx
@@ -1,18 +1,15 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
 
 const LandingPage: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Landing Page</title>
-      </Head>
-
-      <main>
-        <h1>Start your journey!</h1>
-      </main>
-    </div>
+    <main>
+  <div>
+    <h1>
+      Start your journey today!
+    </h1>
+  </div>
+  </main>
   )
 }
 
