8a76f67c062d31f2e16c88d533a36bba7f78f7be
Emil <emil.hagelin@medieinstitutet.se>
2022-12-28 11:12:06 +0100

    added button

--- pages/landingPage.tsx
+++ pages/landingPage.tsx
@@ -3,8 +3,16 @@ export default function LandingPage() {
     <div>
       <div>
         <span>Logo</span>
+        <p>Stay hydrated</p>
       </div>
-      <h1>Water is natures powerup</h1>
+      <h2>Water is natures powerup</h2>
+      <h3>
+        The right amount of water have magical effect on your health, start
+        measuring now
+      </h3>
+      <div>
+        <button>Start your journey</button>
+      </div>
     </div>
   );
 }
