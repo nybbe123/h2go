c8d044a28250b3fb92b443bbf9246ebbef18f81e
Emil <emil.hagelin@medieinstitutet.se>
2022-12-28 11:19:19 +0100

    added font

--- pages/landingPage.tsx
+++ pages/landingPage.tsx
@@ -1,18 +1,22 @@
 export default function LandingPage() {
   return (
-    <div>
-      <div>
-        <span>Logo</span>
-        <p>Stay hydrated</p>
-      </div>
-      <h2>Water is natures powerup</h2>
-      <h3>
-        The right amount of water have magical effect on your health, start
-        measuring now
-      </h3>
-      <div>
-        <button>Start your journey</button>
-      </div>
-    </div>
+    <body>
+      <main>
+        <div>
+          <div>
+            <span>Logo</span>
+            <p>Stay hydrated</p>
+          </div>
+          <h2>Water is natures powerup</h2>
+          <h3>
+            The right amount of water have magical effect on your health, start
+            measuring now
+          </h3>
+          <div>
+            <button>Start your journey</button>
+          </div>
+        </div>
+      </main>
+    </body>
   );
 }
