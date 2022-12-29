18da5bb9a5515dab697988d3a414a9e69b88d6fe
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-12-29 11:54:52 +0100

    adding scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import { getSession, signIn } from "next-auth/react";
 import { GetServerSideProps, NextPage } from "next";
 
