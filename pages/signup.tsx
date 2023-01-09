d8b6ebb6fb0763922c1de4b208e20affd173231f
Emil <emil.hagelin@medieinstitutet.se>
2023-01-09 16:01:14 +0100

    small fix

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -110,7 +110,7 @@ const SignUp: NextPage<
                 id="email"
                 name="email"
                 type="email"
-                placeholder="Skriv din E-mail här"
+                placeholder="sven@example.com"
                 required
                 pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
                 onChange={emailHandler}
