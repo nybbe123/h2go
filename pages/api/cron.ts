701545a5d51f762f08f43cf448de2c4a01891416
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2023-01-10 01:49:49 +0100

    cron test

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -16,7 +16,7 @@ export default async function handler(
           },
         })
         return res.status(200).json({ updateUsers });
-        
+
       } else {
         res.status(401).json({ success: false });
       }
